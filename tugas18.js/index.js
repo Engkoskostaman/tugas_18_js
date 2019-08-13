66fa04b36c09875f68a4e359e8d72669c3272e14
Engkos <engkoskostaman97@gmail.com>
2019-08-13 08:51:16 +0700

    tugas 18 js
    tugas ini kami kumpulkan untuk niomic

---
+++ tugas18.js/index.js
@@ -0,0 +1,8 @@
+function Slice(){
+  var kata="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout";
+
+  console.log(kata.slice(7,24));
+
+
+}
+Slice();
